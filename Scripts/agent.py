aac44c4a24eca9cde1e37ba44e87ada567418f35
Maria Brabham <mattbrabham@hotmail.com>
2021-09-17 15:23:41 +0100

    Agent

---
+++ Scripts/agent.py
@@ -0,0 +1,16 @@
+from worldClass import InteractableObject, Tile
+
+class Agent():
+    def __init__(self, location, params):
+        self.location = location
+        self.paramDictionary = params
+
+        self.inventory = {}
+
+    def Move(self, worldMap):
+        
+        pass
+
+    def PickupItem(self, interactableList):
+
+        pass

2e8306608a7dc67e1321c030828288d7066e9bfe
M. Brabham <mattbrabham@hotmail.com>
2021-11-15 15:27:51 +0000

    Data Logger Class

---
+++ Scripts/datalogger.py
@@ -0,0 +1,55 @@
+import pickle
+
+class DataCollector():
+    def __init__(self, name, dataStructure, load=True):
+        self.name = name
+
+        self.dataStructure = dataStructure
+
+        if load:
+            self.dataPoints = DataColletor.LoadDataPoints(name)
+        else:
+            self.dataPoints = []
+
+    def LogDataPointBatch(self, dataPoints):
+        for i in range(len(dataPoints)):
+            self.LogDataPoint(dataPoints[i])
+
+    def LogDataPoint(self, dataPoint):
+        if self.CheckMatchStructure(dataPoint):
+            self.dataPoints.append(dataPoint)
+
+    def CheckMatchStructure(self, dataPoint):
+        if len(dataPoint) != len(self.dataStructure):
+            raise Exception("Structure of Data Point does not match Collector Specified Structure")
+
+        for i in range(len(dataPoint)):
+            t1 = type(dataPoint[i])
+            t2 = self.dataStructure[i]
+
+            if type(t2) == list: # Checks Multiple types against t1
+                flag = False
+
+                for i in range(len(t2)):
+                    if t1 == t2[i]:
+                        flag = True
+                if flag:
+                    continue
+
+            else:                # Checks Singular type against t1
+                if t1 == t2:
+                    continue
+
+            raise Exception(("Type: {} != Data Structure Type: {} \n {}").format(t1, t2, self.dataStructure))
+        return True
+
+    # Using Pickle to Save/Load
+    @staticmethod
+    def LoadDataPoints(file): # Returns stored Neural Network data
+        with open("DataLogger\\" + self.name + ".data", "rb") as f:
+            temp = pickle.load(f)
+        return temp
+
+    def SaveDataPoints(self, file): # Saves Neural Network Data
+        with open("DataLogger\\" + self.name + ".data", "wb") as f:
+            pickle.dump(self.dataPoints)
